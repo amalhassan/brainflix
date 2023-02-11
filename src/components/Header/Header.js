5f42bbe0cb86afd4bd73d14557ed36b29eab4dd0
Amal Hassan <amalmhass@gmail.com>
2023-02-11 01:46:34 -0500

    added content and styling to the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.scss';
+import Logo from '../../assets/BrainFlix-logo.svg'
+import SearchIcon from '../../assets/icons/search.svg'
+import ProfileImage from '../../assets/images/Mohan-muruge.jpg' 
+import UploadIcon from '../../assets/icons/upload.svg'
+function Header() {
+  return (
+    <div className="header">
+        <div className="header__logo-container">
+            <a href="#"><img className="header__logo" src={Logo} alt="logo"/></a>
+        </div>
+        <div className="header__nav">
+            <div className="header__searchbar">
+                <img className="header__search-icon" src={SearchIcon} alt=""/>
+                <input className="header__input" type="text" placeholder='Search'></input>
+            </div>
+            <img className="header__avatar" src={ProfileImage} alt="avatar"/>
+            <button className="header__upload-button" type="submit"><img className="header__button-icon" src={UploadIcon} alt=""/>Upload</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
